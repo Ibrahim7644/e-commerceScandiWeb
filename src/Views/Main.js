5e85be25653549738ecbcefb03b35afc3ea3babf
unknown <engr.ibrahim649@gmail.com>
2022-04-18 23:03:57 +0500

    set main.js

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,17 +1,41 @@
+
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default class Main extends Component {
+
+    women() {
+        console.log("hello")
+
+    }
+
+
     render() {
         return (
-            <div>
-
-                <nav>
-
-
-                </nav>
+            <div className='row justify-content-center'>
+                <div className='col-3'>
 
 
 
+                    <button onClick={this.women}>WOMEN</button>
+                    <button>MEN</button>
+                    <button>KIDS</button>
+                </div>
+                <div className='col-5'>
+
+
+
+                    <button>WOMEN</button>
+                    <button>MEN</button>
+                    <button>KIDS</button>
+                </div>
+
+
+                <div>
+
+                    hello
+                </div>
             </div>
         )
     }
